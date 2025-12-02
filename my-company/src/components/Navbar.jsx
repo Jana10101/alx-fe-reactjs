87c7009b3cd46864c0b5e21dbfaa0d325d0f2d7f
Jana Elsalhy <janamohamed@Janas-MacBook-Pro.local>
2025-12-02 19:05:18 +0200

    3

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav style={{ backgroundColor: '#282c34', display: 'flex', justifyContent: 'space-around', padding: '1rem' }}>
+      <ul style={{ listStyle: 'none', display: 'flex', gap: '1rem' }}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
