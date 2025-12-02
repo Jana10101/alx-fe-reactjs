19c66d1a41b47ab4d961e5cacaefcf4ac1930b0d
Jana Elsalhy <janamohamed@Janas-MacBook-Pro.local>
2025-12-02 16:01:27 +0200

    1

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './src/components/Header.jsx';
-import MainContent from './src/components/MainContent.jsx';
-import Footer from './src/components/Footer.jsx';
-import UserProfile from './src/components/UserProfile.jsx';
+import Header from './components/Header.jsx'
+import MainContent from './components/MainContent.jsx'
+import Footer from './components/Footer.jsx'
+import UserProfile from './components/UserProfile.jsx'
+import Counter from './components/Counter.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Counter />
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     <Header />
     <MainContent />
